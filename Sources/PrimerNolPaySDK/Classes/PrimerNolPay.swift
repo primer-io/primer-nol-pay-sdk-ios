9f5f5c5138f6ed11c824e7d5cf250a0a33baac1e
Boris Nikolic <boris.nikolic.dev@gmail.com>
2023-10-04 15:58:13 +0200

    Extract PrimerNolPayProtocol for testing purpose

--- Sources/PrimerNolPaySDK/Classes/PrimerNolPay.swift
+++ Sources/PrimerNolPaySDK/Classes/PrimerNolPay.swift
@@ -9,12 +9,48 @@ import Foundation
 import TransitSDK
 import CoreNFC
 
-public class PrimerNolPay {
+public protocol PrimerNolPayProtocol {
+    
+    init(appId: String, isDebug: Bool, isSandbox: Bool, appSecretHandler: @escaping (String, String) async throws -> String)
+    
+    func scanNFCCard(completion: @escaping (Result<String, PrimerNolPayError>) -> Void)
+    
+    func makeLinkingTokenFor(cardNumber: String, completion: @escaping (Result<String, PrimerNolPayError>) -> Void)
+    
+    func sendLinkOTPTo(mobileNumber: String,
+                       withCountryCode countryCode: String,
+                       andToken token: String,
+                       completion: ((Result<Bool, PrimerNolPayError>) -> Void)?)
+    
+    func linkCardFor(otp: String,
+                     andLinkToken linkToken: String,
+                     completion: @escaping (Result<Bool, PrimerNolPayError>) -> Void)
+    
+    func sendUnlinkOTPTo(mobileNumber: String,
+                         withCountryCode countryCode: String,
+                         andCardNumber cardNumber: String,
+                         completion: @escaping (Result<(String, String), PrimerNolPayError>) -> Void)
+    
+    func unlinkCardWith(cardNumber: String,
+                        otp: String,
+                        andUnlinkToken unlinkToken: String,
+                        completion: @escaping (Result<Bool, PrimerNolPayError>) -> Void)
+    
+    func getAvaliableCardsFor(mobileNumber: String,
+                              withCountryCode countryCode: String,
+                              completion: @escaping (Result<[PrimerNolPayCard], PrimerNolPayError>) -> Void)
+    
+    func requestPaymentFor(cardNumber: String,
+                           andTransactionNumber transactionNumber: String,
+                           completion: @escaping (Result<Bool, PrimerNolPayError>) -> Void)
+}
+
+public class PrimerNolPay: PrimerNolPayProtocol {
     
     private let appSecretHandler: (String, String) async throws -> String
     
     /// Initialisation of the wrapper SDK
-    public init(appId: String, isDebug: Bool, isSandbox: Bool, appSecretHandler: @escaping (String, String) async throws -> String) {
+    required public init(appId: String, isDebug: Bool, isSandbox: Bool, appSecretHandler: @escaping (String, String) async throws -> String) {
         
         self.appSecretHandler = appSecretHandler
         
